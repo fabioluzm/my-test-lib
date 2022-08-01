180ed12c32c7cf8f2a8753d9d89434ae31a347fe
fabioluzm <44882970+fabioluzm@users.noreply.github.com>
2022-08-01 23:25:07 +0100

    new styles

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 export interface IButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   backgroundColor?: string;
   color?: string;
+  width?: string;
+  height?: string;
 }
 
-export const Button: React.FunctionComponent<IButtonProps> = ({ children, backgroundColor, color, style, ...props }) => {
+export const Button: React.FunctionComponent<IButtonProps> = ({ children, backgroundColor, color, width, height, style, ...props }) => {
   let _style: React.CSSProperties = style || {};
 
   // override defaults
   if (backgroundColor) _style.backgroundColor = backgroundColor;
   if (color) _style.color = color;
+  if (width) _style.width = width;
+  if (height) _style.height = height;
 
   return (
     <button style={_style} {...props}>
